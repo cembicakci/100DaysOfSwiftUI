5a18f2a1be164b6a820e818fa1ae77723b15defa
Cem Bıçakcı <cmbicakci@gmail.com>
2023-08-02 08:58:57 +0300

    unit conversion

---
+++ UnitConversion/UnitConversion/UnitConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConversionApp.swift
+//  UnitConversion
+//
+//  Created by Cem Bıçakcı on 2.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct UnitConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
