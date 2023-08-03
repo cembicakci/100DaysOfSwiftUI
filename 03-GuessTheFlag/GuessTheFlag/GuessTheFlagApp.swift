a71448158fb8e0f7d51bbf17a168775aae9baf3c
Cem Bıçakcı <cmbicakci@gmail.com>
2023-08-03 13:54:46 +0300

    guess the flag

---
+++ 03-GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Cem Bıçakcı on 3.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
