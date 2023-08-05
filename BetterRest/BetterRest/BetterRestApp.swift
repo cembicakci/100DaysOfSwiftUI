c1a074bfc0521bfc9a9ef129ec552369f8e799fc
Cem Bıçakcı <cmbicakci@gmail.com>
2023-08-05 19:08:47 +0300

    better rest

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Cem Bıçakcı on 5.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BetterRest/BetterRest/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Cem Bıçakcı on 5.08.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var sleepAmount = 8.0
+    
+    var body: some View {
+        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
