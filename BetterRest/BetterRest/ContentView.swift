ab0bf1dfb555e766c741aa1c74dedf44938cd064
Cem Bıçakcı <cmbicakci@gmail.com>
2023-08-05 19:47:06 +0300

    implemented coreml

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -8,11 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var sleepAmount = 8.0
+    @State private var wakeUp = Date.now
     
     var body: some View {
-        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+        DatePicker("Please enter a date", selection: $wakeUp, displayedComponents: .hourAndMinute)
+            .labelsHidden()
     }
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
