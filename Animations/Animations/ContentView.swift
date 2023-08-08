95cbdb244d9da8bc0842d047b8910489f0c0ac54
Cem Bıçakcı <cmbicakci@gmail.com>
2023-08-08 21:13:16 +0300

    animations

---
+++ Animations/Animations/ContentView.swift
@@ -0,0 +1,51 @@
+//
+//  ContentView.swift
+//  Animations
+//
+//  Created by Cem Bıçakcı on 8.08.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let letters = Array("Hello, SwiftUI")
+    @State private var enabled = false
+    @State private var dragAmount = CGSize.zero
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            
+            HStack(spacing: 0) {
+                ForEach(0..<letters.count) { num in
+                    Text(String(letters[num]))
+                        .padding(5)
+                        .font(.title)
+                        .background(enabled ? .blue : .red)
+                        .offset(dragAmount)
+                        .animation(
+                            .default.delay(Double(num) / 20),
+                            value: dragAmount
+                        )
+                }
+            }
+            .gesture(
+                DragGesture()
+                    .onChanged { dragAmount = $0.translation }
+                    .onEnded { _ in
+                        dragAmount = .zero
+                        enabled.toggle()
+                    }
+            )
+        
+            Spacer()
+        
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
