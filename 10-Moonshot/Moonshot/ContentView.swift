464a99af446e34e06f76d30daeee545f31fa4398
Cem Bıçakcı <cmbicakci@gmail.com>
2023-08-14 21:20:06 +0300

    created bundle

--- 10-Moonshot/Moonshot/ContentView.swift
+++ 10-Moonshot/Moonshot/ContentView.swift
@@ -8,9 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let astronauts = Bundle.main.decode("astronauts.json")
 
     var body: some View {
-        Text("Item")
+        Text("\(astronauts.count)")
     }
 }
 
