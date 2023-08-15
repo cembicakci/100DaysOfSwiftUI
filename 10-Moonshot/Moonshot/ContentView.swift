4efb397f2b6bdb72aa1512a973cc45f37b12288e
Cem Bıçakcı <cmbicakci@gmail.com>
2023-08-15 21:59:19 +0300

    added grid/list view

--- 10-Moonshot/Moonshot/ContentView.swift
+++ 10-Moonshot/Moonshot/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingGrid = false
     
     let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
     let missions: [Mission] = Bundle.main.decode("missions.json")
@@ -19,16 +20,49 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             ScrollView {
-                LazyVGrid(columns: columns) {
+                if showingGrid {
+                    LazyVGrid(columns: columns) {
+                        ForEach(missions) { mission in
+                            NavigationLink {
+                                MissionView(mission: mission, astronauts: astronauts)
+                            } label: {
+                                VStack {
+                                    Image(mission.image)
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 100, height: 100)
+                                        .padding()
+                                    
+                                    VStack {
+                                        Text(mission.name)
+                                            .font(.headline)
+                                            .foregroundColor(.white)
+                                        
+                                        Text(mission.formattedLaunchDate)
+                                            .font(.caption)
+                                            .foregroundColor(.white.opacity(0.5))
+                                    }
+                                    .padding(.vertical)
+                                    .frame(maxWidth: .infinity)
+                                    .background(.lightBackground)
+                                }
+                                .clipShape(RoundedRectangle(cornerRadius: 10))
+                                .overlay(RoundedRectangle(cornerRadius: 10)
+                                    .stroke(.lightBackground))
+                            }
+                        }
+                    }
+                    .padding([.horizontal, .bottom])
+                } else {
                     ForEach(missions) { mission in
                         NavigationLink {
                             MissionView(mission: mission, astronauts: astronauts)
                         } label: {
-                            VStack {
+                            HStack {
                                 Image(mission.image)
                                     .resizable()
                                     .scaledToFit()
-                                    .frame(width: 100, height: 100)
+                                    .frame(width: 50, height: 50)
                                     .padding()
                                 
                                 VStack {
@@ -41,7 +75,7 @@ struct ContentView: View {
                                         .foregroundColor(.white.opacity(0.5))
                                 }
                                 .padding(.vertical)
-                                .frame(maxWidth: .infinity)
+                                .frame(maxWidth: .infinity, maxHeight: .infinity)
                                 .background(.lightBackground)
                             }
                             .clipShape(RoundedRectangle(cornerRadius: 10))
@@ -49,12 +83,19 @@ struct ContentView: View {
                                 .stroke(.lightBackground))
                         }
                     }
+                    .padding(.horizontal)
                 }
-                .padding([.horizontal, .bottom])
             }
             .navigationTitle("Moonshot")
             .background(.darkBackground)
             .preferredColorScheme(.dark)
+            .toolbar {
+                Button {
+                    showingGrid = !showingGrid
+                } label: {
+                    Text(showingGrid ? "Grid View" : "List View")
+                }
+            }
         }
     }
 }
