aab45615a8053e94fbb594b1319ae84ff91a5d43
Cem Bıçakcı <cmbicakci@gmail.com>
2023-08-22 20:21:39 +0300

    habito app first

---
+++ 12-HabitoApp/HabitoApp/HabitoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitoAppApp.swift
+//  HabitoApp
+//
+//  Created by Cem Bıçakcı on 22.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct HabitoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
