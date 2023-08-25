2d8635bcdad9a980317d08e573dc0930646b0f1e
Cem Bıçakcı <cmbicakci@gmail.com>
2023-08-25 20:01:02 +0300

    cupcake corner

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Cem Bıçakcı on 25.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
