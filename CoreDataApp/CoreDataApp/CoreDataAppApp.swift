00498c5d23a82e8ee91a339b2cf88fd3cecc226f
Cem Bıçakcı <cmbicakci@gmail.com>
2023-09-02 13:57:48 +0300

    coredata

---
+++ CoreDataApp/CoreDataApp/CoreDataAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataAppApp.swift
+//  CoreDataApp
+//
+//  Created by Cem Bıçakcı on 2.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
