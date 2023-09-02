ef04fa2d26ccf1835d0cf8e5c520b14a3c7e0ff5
Cem Bıçakcı <cmbicakci@gmail.com>
2023-09-02 15:09:51 +0300

    singer fetch request

---
+++ CoreDataApp/CoreDataApp/Singer+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Singer+CoreDataClass.swift
+//  CoreDataApp
+//
+//  Created by Cem Bıçakcı on 2.09.2023.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Singer)
+public class Singer: NSManagedObject {
+
+}
