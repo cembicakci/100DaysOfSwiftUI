d5d5cb26e351edb72f39030974faf2656cc8e113
Cem Bıçakcı <cmbicakci@gmail.com>
2023-09-02 14:53:12 +0300

    nsfetch

---
+++ CoreDataApp/Movie+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Movie+CoreDataClass.swift
+//  CoreDataApp
+//
+//  Created by Cem Bıçakcı on 2.09.2023.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Movie)
+public class Movie: NSManagedObject {
+
+}
---
+++ CoreDataApp/Movie+CoreDataProperties.swift
@@ -0,0 +1,27 @@
+//
+//  Movie+CoreDataProperties.swift
+//  CoreDataApp
+//
+//  Created by Cem Bıçakcı on 2.09.2023.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Movie {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Movie> {
+        return NSFetchRequest<Movie>(entityName: "Movie")
+    }
+
+    @NSManaged public var title: String?
+    @NSManaged public var director: String?
+    @NSManaged public var year: Int16
+
+}
+
+extension Movie : Identifiable {
+
+}
