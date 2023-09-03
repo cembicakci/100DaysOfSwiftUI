e85c91bb8d3924b31ff7d94aeb7306ab14b97cd8
Cem Bıçakcı <cmbicakci@gmail.com>
2023-09-03 13:26:41 +0300

    fetch users

--- Friendface/Friendface/ContentView.swift
+++ Friendface/Friendface/ContentView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var users = [User]()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List(users) { user in
+                NavigationLink {
+                    Text(user.name)
+                } label: {
+                    HStack {
+                        Circle()
+                            .fill(user.isActive ? .green : .red)
+                            .frame(width: 30)
+                        
+                        Text(user.name)
+                    }
+                }
+            }
+            .navigationTitle("Friendface")
+            .task {
+                await fetchUsers()
+            }
         }
-        .padding()
+    }
+    
+    func fetchUsers() async {
+        guard users.isEmpty else { return }
+        
+        do {
+            let url = URL(string: "https://www.hackingwithswift.com/samples/friendface.json")!
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            let decoder = JSONDecoder()
+            decoder.dateDecodingStrategy = .iso8601
+            
+            users = try decoder.decode([User].self, from: data)
+        } catch {
+            print("Download failed")
+        }
     }
 }
 
