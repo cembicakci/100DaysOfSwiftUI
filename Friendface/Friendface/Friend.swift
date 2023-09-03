969034d7ac89b6e934d841e6a45512300c800972
Cem Bıçakcı <cmbicakci@gmail.com>
2023-09-03 13:18:05 +0300

    user and friend model

---
+++ Friendface/Friendface/Friend.swift
@@ -0,0 +1,13 @@
+//
+//  Friend.swift
+//  Friendface
+//
+//  Created by Cem Bıçakcı on 3.09.2023.
+//
+
+import Foundation
+
+struct Friend: Identifiable, Codable {
+    let id: UUID
+    let name: String
+}
---
+++ Friendface/Friendface/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  Friendface
+//
+//  Created by Cem Bıçakcı on 3.09.2023.
+//
+
+import Foundation
+
+struct User: Identifiable, Codable {
+    let id: UUID
+    let isActive: Bool
+    let name: String
+    let age: Int
+    let company: String
+    let email: String
+    let address: String
+    let about: String
+    let registered: Date
+    let tags: [String]
+    let friends: [Friend]
+}
