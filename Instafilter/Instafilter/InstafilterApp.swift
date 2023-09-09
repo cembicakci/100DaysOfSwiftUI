ec8a510dc97744d35badbcd99be805f76ad868ea
Cem Bıçakcı <cmbicakci@gmail.com>
2023-09-09 10:55:24 +0300

    instafilter

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Cem Bıçakcı on 9.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
