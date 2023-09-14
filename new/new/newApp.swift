52629f5ff21205c0bde06e928bfc7a4a0f9d431b
Cem Bıçakcı <cmbicakci@gmail.com>
2023-09-14 22:14:32 +0300

    18th project

---
+++ new/new/newApp.swift
@@ -0,0 +1,17 @@
+//
+//  newApp.swift
+//  new
+//
+//  Created by Cem Bıçakcı on 14.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct newApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
