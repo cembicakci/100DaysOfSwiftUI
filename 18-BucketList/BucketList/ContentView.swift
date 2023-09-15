f3d6fe77990e39fcfd6cb46e2759baed0615f633
Cem Bıçakcı <cmbicakci@gmail.com>
2023-09-15 20:30:51 +0300

    comparable

--- 18-BucketList/BucketList/ContentView.swift
+++ 18-BucketList/BucketList/ContentView.swift
@@ -7,16 +7,28 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+struct User: Identifiable, Comparable {
+    let id = UUID()
+    let firstName: String
+    let lastName: String
+
+    static func <(lhs: User, rhs: User) -> Bool {
+        lhs.lastName < rhs.lastName
     }
+}
+
+struct ContentView: View {
+    let users = [
+        User(firstName: "Arnold", lastName: "Rimmer"),
+        User(firstName: "Kristine", lastName: "Kochanski"),
+        User(firstName: "David", lastName: "Lister"),
+    ].sorted()
+
+   var body: some View {
+       List(users) { user in
+           Text("\(user.lastName), \(user.firstName)")
+       }
+   }
 }
 
 struct ContentView_Previews: PreviewProvider {
