454b762cab54d27880945b36a379a6438c6c8279
Cem Bıçakcı <cmbicakci@gmail.com>
2023-09-17 13:17:11 +0300

    file-manager

--- 18-BucketList/BucketList/ContentView-ViewModel.swift
+++ 18-BucketList/BucketList/ContentView-ViewModel.swift
@@ -11,14 +11,33 @@ import Foundation
 extension ContentView {
     @MainActor class ViewModel: ObservableObject {
         @Published var mapRegion  = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 50, longitude: 0), span: MKCoordinateSpan(latitudeDelta: 25, longitudeDelta: 25))
-        
-        @Published private(set) var locations = [Location]()
-        
+        @Published private(set) var locations: [Location]
         @Published var selectedPlace: Location?
+        
+        let savePath = FileManager.documentDirectory.appendingPathExtension("SavedPlaces")
+        
+        init() {
+            do {
+                let data = try Data(contentsOf: savePath)
+                locations = try JSONDecoder().decode([Location].self, from: data)
+            } catch {
+                locations = []
+            }
+        }
+        
+        func save() {
+            do {
+                let data = try JSONEncoder().encode(locations)
+                try data.write(to: savePath, options: [.atomicWrite,. completeFileProtection])
+            } catch {
+                print("Unable to save data.")
+            }
+        }
         
         func addLocation() {
             let newLocation = Location(id: UUID(), name: "New Location", description: "", latitude: mapRegion.center.latitude, longitude: mapRegion.center.longitude)
             locations.append(newLocation)
+            save()
         }
         
         func update(location: Location) {
@@ -26,6 +45,7 @@ extension ContentView {
             
             if let index = locations.firstIndex(of: selectedPlace) {
                 locations[index] = location
+                save()
             }
         }
     }
