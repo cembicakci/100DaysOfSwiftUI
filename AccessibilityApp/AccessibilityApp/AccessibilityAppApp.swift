3dfbe6e17d04621febce333f5e5fb2939ef48243
Cem Bıçakcı <cmbicakci@gmail.com>
2023-09-19 20:10:11 +0300

    accessibility app

---
+++ AccessibilityApp/AccessibilityApp/AccessibilityAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilityAppApp.swift
+//  AccessibilityApp
+//
+//  Created by Cem Bıçakcı on 19.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilityAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
