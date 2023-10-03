a4dcdec367f8c8a7cb4f46a3eac633c3073e05a7
Cem Bıçakcı <cmbicakci@gmail.com>
2023-10-03 20:24:48 +0300

    enviroment pbject

---
+++ 21-HotProspects/HotProspects/Prospect.swift
@@ -0,0 +1,23 @@
+//
+//  Prospect.swift
+//  HotProspects
+//
+//  Created by Cem Bıçakcı on 3.10.2023.
+//
+
+import SwiftUI
+
+class Prospect: Identifiable, Codable {
+    var id = UUID()
+    var name = "Anonymous"
+    var emailAddress = ""
+    var isContacted = false
+}
+
+@MainActor class Prospects: ObservableObject {
+    @Published var people: [Prospect]
+    
+    init() {
+        people = []
+    }
+}
