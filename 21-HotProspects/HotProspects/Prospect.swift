4d2cab9ed6dc107af77e8280fe67ec0173e8eaa3
Cem Bıçakcı <cmbicakci@gmail.com>
2023-10-09 20:42:23 +0300

    user default

--- 21-HotProspects/HotProspects/Prospect.swift
+++ 21-HotProspects/HotProspects/Prospect.swift
@@ -17,14 +17,34 @@ class Prospect: Identifiable, Codable {
 }
 
 @MainActor class Prospects: ObservableObject {
-    @Published var people: [Prospect]
+    @Published private(set) var people: [Prospect]
     
     init() {
+        if let data = UserDefaults.standard.data(forKey: "SavedData") {
+            if let decoded = try? JSONDecoder().decode([Prospect].self, from: data) {
+                people = decoded
+                return
+            }
+        }
+        //no saved data
         people = []
     }
     
     func toggle(_ prospect: Prospect) {
         objectWillChange.send()
         prospect.isContacted.toggle()
+        save()
+    }
+    
+    func add(_ prospect: Prospect) {
+        people.append(prospect)
+        save()
+    }
+    
+    private func save() {
+        if let encoded = try? JSONEncoder().encode(people) {
+            UserDefaults.standard.set(encoded, forKey: "SavedData")
+        }
+            
     }
 }
