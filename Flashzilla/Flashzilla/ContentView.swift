9edbe5b0ad327f966e32621a669254c0dc626cc4
Cem Bıçakcı <cmbicakci@gmail.com>
2023-10-11 18:45:45 +0300

    card view

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -8,39 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var currentAmount = Angle.zero
-    @State private var finalAmount = Angle.zero
-    
-    @State private var currentAmount2 = 0.0
-    @State private var finalAmount2 = 1.0
-
     
     var body: some View {
-        Text("Hello, World!")
-           .rotationEffect(currentAmount + finalAmount)
-           .gesture(
-               RotationGesture()
-                   .onChanged { angle in
-                       currentAmount = angle
-                   }
-                   .onEnded { angle in
-                       finalAmount += currentAmount
-                       currentAmount = .zero
-                   }
-           )
-            
-        Text("Hello, World!")
-          .scaleEffect(finalAmount2 + currentAmount2)
-          .gesture(
-              MagnificationGesture()
-                  .onChanged { amount in
-                      currentAmount2 = amount - 1
-                  }
-                  .onEnded { amount in
-                      finalAmount2 += currentAmount2
-                      currentAmount2 = 0
-                  }
-          )
+        CardView(card: Card.example)
     }
 }
 
